c15a97007c95a616709bc21ffa01e5ebc4a50f21
Lyron Co Ting Keh <lyronctk@stanford.edu>
2023-08-01 18:55:54 +0100

    player A, B, C spawning

--- enclave/main.ts
+++ enclave/main.ts
@@ -32,6 +32,7 @@ class Tile {
 
 class Grid {
   t: Tile[][];
+  unowned: Player;
 
   constructor(sz: number, unowned: Player) {
     this.t = new Array<Array<Tile>>();
@@ -42,13 +43,18 @@ class Grid {
       }
       this.t.push(row);
     }
+    this.unowned = unowned;
   }
 
-  // spawn(self, loc: [number, number], pl: Player) {
-  //   if(loc[0] >= self.t.length || loc[0] < 0 || loc[1] >= self.t[0].length || loc[1] < 0) {
-  //     throw new E
-  //   }
-  // }
+  spawn(r: number, c: number, pl: Player, resource: number) {
+    if(r >= this.t.length || r < 0 || c >= this.t[0].length || r < 0) {
+      throw new Error("Tried to spawn player out of bounds.")
+    }
+    if(this.t[r][c].owner != this.unowned) {
+      throw new Error("Tried to spawn player on an owned tile.");
+    }
+    this.t[r][c] = new Tile(pl, resource, Utils.randFQ());
+  }
 
   toString(): string {
     return this.t
@@ -59,10 +65,19 @@ class Grid {
 
 const GRID_SIZE: number = 5;
 const START_RESOURCES: number = 9;
+
 const UNOWNED: Player = new Player("_");
+const PLAYER_A: Player = new Player("A");
+const PLAYER_B: Player = new Player("B");
+const PLAYER_C: Player = new Player("C");
 
 (async () => {
   const g = new Grid(GRID_SIZE, UNOWNED);
+  
+  g.spawn(0, 0, PLAYER_A, START_RESOURCES);
+  g.spawn(0, GRID_SIZE - 1, PLAYER_B, START_RESOURCES);
+  g.spawn(GRID_SIZE - 1, 0, PLAYER_C, START_RESOURCES);
+
   console.log(g.toString());
   process.exit(0);
 })();
