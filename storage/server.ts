e37660515fd41d615b5f0bb1a249cc5cdcc56c9b
Alexander Lindenbaum <lindenbaum.alexander@gmail.com>
2023-10-12 10:30:56 -0400

    db and pg working

--- storage/server.ts
+++ storage/server.ts
@@ -1,4 +1,5 @@
 import { io, Socket } from "socket.io-client";
+import { Client } from "pg";
 import dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
 import { ServerToClientEvents, ClientToServerEvents } from "../enclave/socket";
@@ -9,6 +10,12 @@ import { ServerToClientEvents, ClientToServerEvents } from "../enclave/socket";
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
     `http://localhost:${process.env.ENCLAVE_SERVER_PORT}`
 );
+
+/*
+ * Interface with Postgres database. Default constructor reads from
+ * environment variables.
+ */
+const client = new Client();
 
 function handshakeDAResponse() {
     console.log("handshake completed!");
@@ -27,6 +34,14 @@ function updateDA(sender: string, ciphertext: string, iv: string, tag: string) {
 
 socket.on("connect", async () => {
     console.log("Connection with enclave node established");
+
+    await client.connect();
+
+    const res = await client.query("SELECT $1::text as message", [
+        "Hello world!",
+    ]);
+    console.log(res.rows[0].message); // Hello world!
+    await client.end();
 
     socket.emit("handshakeDA");
 });
