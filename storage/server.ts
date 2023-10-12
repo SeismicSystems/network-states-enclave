1797a2da2feae5843ecfabd6448268da98b9cb79
Alexander Lindenbaum <lindenbaum.alexander@gmail.com>
2023-10-12 15:12:34 -0400

    insert encrypted tiles and clear table

--- storage/server.ts
+++ storage/server.ts
@@ -1,5 +1,5 @@
 import { io, Socket } from "socket.io-client";
-import { Client } from "pg";
+import { Pool } from "pg";
 import dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
 import { ServerToClientEvents, ClientToServerEvents } from "../enclave/socket";
@@ -15,38 +15,52 @@ const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
  * Interface with Postgres database. Default constructor reads from
  * environment variables.
  */
-const client = new Client();
+const pool = new Pool();
 
 function handshakeDAResponse() {
     console.log("handshake completed!");
 }
 
-function pushToDA(sender: string, ciphertext: string, iv: string, tag: string) {
-    // [TODO]: add ciphertext to db
-    console.log("NewEntry {");
-    console.log("   sender: ", sender + ",");
-    console.log("   ciphertext: ", ciphertext + ",");
-    console.log("   iv: ", iv + ",");
-    console.log("   tag: ", tag);
-    console.log("}");
+/*
+ * Adds encrypted tile as row into database.
+ */
+async function pushToDA(
+    sender: string,
+    ciphertext: string,
+    iv: string,
+    tag: string
+) {
+    const client = await pool.connect();
+
+    await client.query(
+        `INSERT INTO encrypted_tiles (sender, ciphertext, iv, tag)
+        VALUES ($1, $2, $3, $4)`,
+        [sender, ciphertext, iv, tag]
+    );
+
+    client.release();
+
     socket.emit("pushToDAResponse");
 }
 
 function pullFromDA() {
-    // [TODO]: get, delete and send to enclave: first entry of table
+    // [TODO]: send to enclave: first entry of table
     socket.emit("pullFromDAResponse", true, undefined);
+}
+
+/*
+ * Clears the table of past encrypted tiles.
+ */
+async function clearTable() {
+    const client = await pool.connect();
+    await client.query("TRUNCATE TABLE encrypted_tiles");
+    client.release();
 }
 
 socket.on("connect", async () => {
     console.log("Connection with enclave node established");
 
-    await client.connect();
-
-    const res = await client.query("SELECT $1::text as message", [
-        "Hello world!",
-    ]);
-    console.log(res.rows[0].message); // Hello world!
-    await client.end();
+    await clearTable();
 
     socket.emit("handshakeDA");
 });
