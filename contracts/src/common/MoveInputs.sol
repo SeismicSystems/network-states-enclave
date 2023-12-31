74fd99bc6e6d6c96ce78a9b112d55a360599062b
smallbraingames <smallbraindev@gmail.com>
2023-12-31 12:42:47 -0800

    feat: perform lazy updates before movement

--- contracts/src/common/MoveInputs.sol
+++ contracts/src/common/MoveInputs.sol
@@ -1,6 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+struct Move {
+    bool fromIsCityCenter;
+    bool toIsCityCenter;
+    bool fromIsWaterTile;
+    bool toIsWaterTile;
+    bool takingCity;
+    bool ontoSelfOrUnowned;
+    uint24 fromCityId;
+    uint24 toCityId;
+    uint32 numTroopsMoved;
+    uint32 enemyLoss;
+    uint256 currentInterval;
+    uint256 hTFrom;
+    uint256 hTTo;
+    uint256 hUFrom;
+    uint256 hUTo;
+}
+
+
 struct MoveInputs {
     bool fromIsCityCenter;
     bool toIsCityCenter;
